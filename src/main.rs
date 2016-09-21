fdfc78b4c7dcc2a1f5843cd3bc74c6c150c32baf
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-21 17:54:07 +0900

    add a test

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,63 @@ extern crate regex;
 use std::io::BufRead;
 use regex::Regex;
 
-fn read_lines() -> Vec<String> {
+pub fn read_lines<R: BufRead>(reader: R) -> Vec<String> {
   let ansi = Regex::new(r"\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]").unwrap();
-  let stdin = std::io::stdin();
-  let lines = stdin.lock()
-    .lines()
+  let lines = reader.lines()
     .map(|line| ansi.replace_all(&line.unwrap(), ""))
+    .filter_map(|line| {
+      let trimmed = line.trim();
+      if trimmed.len() != 0 {
+        Some(trimmed.to_owned())
+      } else {
+        None
+      }
+    })
     .collect();
   lines
 }
 
 fn main() {
-  let lines = read_lines();
+  let stdin = std::io::stdin();
+  let lines = read_lines(stdin.lock());
   let mut cli = pecors::Client::new(lines);
 
   let selected = cli.select_line().unwrap();
   match selected {
     Some(line) => println!("{}", line),
     None => (),
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use std::io::{self, Cursor};
+
+  #[test]
+  fn read_lines_simple_case() {
+    let input = r#"
+hogehoge
+征夷大将軍
+"#;
+    let lines = read_lines(io::BufReader::new(Cursor::new(input.as_bytes())));
+
+    assert_eq!(lines.len(), 2);
+    assert_eq!(lines[0], "hogehoge");
+    assert_eq!(lines[1], "征夷大将軍");
+  }
+
+  #[test]
+  fn read_lines_includes_ansi_code() {
+    let input = "
+\x1b[0m\x1b[01;34msrc\x1b[0m/
+\x1b[01;34mtarget\x1b[0m/
+\x1b[0m[01;34m💰\x1b[0m
+";
+    let lines = read_lines(io::BufReader::new(Cursor::new(input.as_bytes())));
+
+    assert_eq!(lines[0], "src/");
+    assert_eq!(lines[1], "target/");
+    assert_eq!(lines[2], "💰");
   }
 }
